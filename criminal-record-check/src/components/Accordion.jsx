740ee834f02583b1d021b2d5a5aea9187aca6008
Rekha Bhagavatula <rekhabhagavatula89@gmail.com>
2023-06-22 09:50:31 +1000

    Added in styles for the accordion

--- criminal-record-check/src/components/Accordion.jsx
+++ criminal-record-check/src/components/Accordion.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
-
+import './style.css'
 const Accordion = ({ title, content }) => {
   const [isActive, setIsActive] = useState(false);
 
   return (
     <div className="accordion-item">
       <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
-        <div>{title}</div>
-        <div>{isActive ? 'V' : '>'}</div>
+        <div>{title} {isActive ? ` V` : ` >`}</div>
       </div>
       {isActive && <div className="accordion-content">{content}</div>}
     </div>
