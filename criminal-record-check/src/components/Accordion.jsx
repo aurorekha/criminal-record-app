4008cb019535ea6002865abe9c8205df12f90602
Rekha Bhagavatula <rekhabhagavatula89@gmail.com>
2023-06-22 23:43:36 +1000

    Data and other things
    Added in data, styles and test to wrap this up

--- criminal-record-check/src/components/Accordion.jsx
+++ criminal-record-check/src/components/Accordion.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import './style.css'
-const Accordion = ({ title, content }) => {
+const Accordion = ({ title, count, descriptionOne, descriptionTwo, descriptionThree, postCode, reportedDate }) => {
   const [isActive, setIsActive] = useState(false);
-
   return (
     <div className="accordion-item">
-      <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
-        <div>{title} {isActive ? ` V` : ` >`}</div>
+      <div onClick={() => setIsActive(!isActive)}>
+        <div className="accordion-title-text">{title} {isActive ? <span>&#8593;</span> : <span>&#8595;</span>}</div>
       </div>
-      {isActive && <div className="accordion-content">{content}</div>}
+        {isActive && 
+        <div className="accordion-content">
+          <p className="para-styles">Reported Date: {reportedDate}</p> 
+          <p className="para-styles">Crime count: {count}</p> 
+          <p className="para-styles">Offence level 1 description: {descriptionOne}</p>  
+          <p className="para-styles">Offence level 2 description: {descriptionTwo} </p> 
+          <p className="para-styles">Offence level 3 description: {descriptionThree} </p>
+          <p className="para-styles">Postcode: {postCode}</p>
+      </div>}
     </div>
   );
 };
