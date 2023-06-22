afc37b808832a9c3f38e8831bb695d73d739f9cb
Rekha Bhagavatula <rekhabhagavatula89@gmail.com>
2023-06-22 10:25:00 +1000

    made the data into setState ready to be used

--- criminal-record-check/src/App.js
+++ criminal-record-check/src/App.js
@@ -1,33 +1,36 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Accordion from './components/Accordion';
 
 const App = () => {
-  // const [accordionData, setAccordionData] = useState([])
-  const accordionData = [
-    {
-      title: 'Header 1',
-      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-      laborum cupiditate possimus labore, hic temporibus velit dicta earum
-      suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-      voluptatem.`
-    },
-    {
-      title: 'Header 2',
-      content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
-      reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
-      quaerat iure quos dolorum accusantium ducimus in illum vero commodi
-      pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
-      quidem maiores doloremque est numquam praesentium eos voluptatem amet!
-      Repudiandae, mollitia id reprehenderit a ab odit!`
-    },
-    {
-      title: 'Header 3',
-      content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
-      quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
-      dolor ut sequi minus iste? Quas?`
-    }
-  ];
-
+  const [accordionData, setAccordionData] = useState([])
+  
+  useEffect(() => {
+    setAccordionData([
+      {
+        title: 'Header 1',
+        content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+        laborum cupiditate possimus labore, hic temporibus velit dicta earum
+        suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+        voluptatem.`
+      },
+      {
+        title: 'Header 2',
+        content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
+        reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
+        quaerat iure quos dolorum accusantium ducimus in illum vero commodi
+        pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
+        quidem maiores doloremque est numquam praesentium eos voluptatem amet!
+        Repudiandae, mollitia id reprehenderit a ab odit!`
+      },
+      {
+        title: 'Header 3',
+        content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
+        quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
+        dolor ut sequi minus iste? Quas?`
+      }
+    ]);
+  
+  }, [])
   return (
     <div>
       <h1>React accordion</h1>
